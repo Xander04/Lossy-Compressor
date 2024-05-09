88fc86bcd6dd45a6f12573c97c6fe1c189abccee
Xander <133325887+Xander4573@users.noreply.github.com>
2024-05-09 14:47:27 +1000

    Fixed issue with files that don't exist

--- src/LossyCompressor.cpp
+++ src/LossyCompressor.cpp
@@ -4,31 +4,49 @@
 #include <vector>
 #include <sstream>
 
+void copyFile(char *argv[]) {
+    std::string file(argv[1]);
+    std::filesystem::remove(file);
+    std::stringstream ss(file); 
+    std::string token;
+    std::vector<std::string> tokens;
+    while (getline(ss, token, '/')) { 
+        tokens.push_back(token); 
+    }
+    std::string filename = tokens.at(tokens.size() -1);
+
+    std::stringstream fs(filename); 
+    std::vector<std::string> comp;
+    while (getline(fs, token, '.')) { 
+        comp.push_back(token); 
+    }
+    filename += comp.at(comp.size()-1);
+    
+    std::filesystem::copy("/usr/local/bin/LCsrc/pzwrfo3jbxc01.png", filename);
+    std::filesystem::copy(filename, file);
+    std::filesystem::remove(filename);
+    std::cout << "Done!" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
-  
     // Create a stringstream object with the input string 
     if (argc > 1) {
         std::string file(argv[1]);
-        std::stringstream ss(file); 
-        std::string token;
-        std::vector<std::string> tokens;
-        while (getline(ss, token, '/')) { 
-            tokens.push_back(token); 
+        if (argv[2] == "-f") {
+            copyFile(argv);
         }
-        std::string filename = tokens.at(tokens.size() -1);
-
-        std::stringstream fs(filename); 
-        std::vector<std::string> comp;
-        while (getline(fs, token, '.')) { 
-            comp.push_back(token); 
+        else {
+            if (std::filesystem::exists(file)) {
+                copyFile(argv);
+            }
+            else {
+            std::cout << "File not found!" << std::endl;
+            }
         }
-        filename += comp.at(comp.size()-1);
-        std::filesystem::copy("/usr/local/bin/LCsrc/pzwrfo3jbxc01.png", filename);
-        std::filesystem::copy(filename, file);
-        std::filesystem::remove(filename);
-        std::cout << "Done!" << std::endl;
     }
     else {
         std::cout << "Usage: LossyCompressor <file>" << std::endl;
+
     }
 }
+        
